b087a550efa0055052aeec920783ada54165ae9c
施从政 <shicongzheng@dcloud.io>
2026-01-16 15:42:58 +0800

    tsgo-dcloud: 文件系统支持复用已有缓存数据

--- io/dcloud/vfs.go
+++ io/dcloud/vfs.go
@@ -3,6 +3,7 @@ package dcloud
 import (
 	"time"
 
+	"github.com/microsoft/typescript-go/internal/ast"
 	"github.com/microsoft/typescript-go/internal/vfs"
 	"github.com/microsoft/typescript-go/internal/vfs/osvfs"
 )
@@ -12,67 +13,77 @@ type VirtualFS interface{
 	ReadFile(path string) (contents string, ok bool);
 }
 
+type Cache interface{
+	GetSourceFile(filename string) *ast.SourceFile;
+}
 
-func NewVirtualFileSystem(fs VirtualFS) vfs.FS {
-	return &virtualFileSystem{fs: fs}
+
+func NewVirtualFileSystem(fs VirtualFS, cache Cache) vfs.FS {
+	return &virtualFileSystem{fs: fs, cache: cache}
 }
 
 type virtualFileSystem struct{
 	fs VirtualFS
+	cache Cache
 }
 
 var _ vfs.FS = (*virtualFileSystem)(nil)
 
 var osVFS = osvfs.FS()
 
-func (vfs *virtualFileSystem) UseCaseSensitiveFileNames() bool {
+func (*virtualFileSystem) UseCaseSensitiveFileNames() bool {
 	return osVFS.UseCaseSensitiveFileNames()
 }
 
-func (vfs *virtualFileSystem) FileExists(path string) bool {
-	if vfs.fs != nil && vfs.fs.FileExists(path) {
+func (v *virtualFileSystem) FileExists(path string) bool {
+	if v.fs != nil && v.fs.FileExists(path) {
 		return true
 	}
 	return osVFS.FileExists(path)
 }
 
-func (vfs *virtualFileSystem) ReadFile(path string) (contents string, ok bool) {
-	if vfs.fs != nil {
-		if contents, ok = vfs.fs.ReadFile(path) ; ok {
+func (v *virtualFileSystem) ReadFile(path string) (contents string, ok bool) {
+	if v.fs != nil {
+		if contents, ok = v.fs.ReadFile(path) ; ok {
 			return contents, ok
+		}
+	}
+	if v.cache != nil {
+		if sf := v.cache.GetSourceFile(path); sf != nil {
+			return sf.Text(), true
 		}
 	}
 	return osVFS.ReadFile(path)
 }
 
-func (vfs *virtualFileSystem) WriteFile(path string, data string, writeByteOrderMark bool) error {
+func (v *virtualFileSystem) WriteFile(path string, data string, writeByteOrderMark bool) error {
 	return osVFS.WriteFile(path, data, writeByteOrderMark)
 }
 
-func (vfs *virtualFileSystem) Remove(path string) error {
+func (v *virtualFileSystem) Remove(path string) error {
 	return osVFS.Remove(path)
 }
 
-func (vfs *virtualFileSystem) Chtimes(path string, aTime time.Time, mTime time.Time) error {
+func (v *virtualFileSystem) Chtimes(path string, aTime time.Time, mTime time.Time) error {
 	return osVFS.Chtimes(path, aTime, mTime)
 }
 
-func (vfs *virtualFileSystem) DirectoryExists(path string) bool {
+func (v *virtualFileSystem) DirectoryExists(path string) bool {
 	return osVFS.DirectoryExists(path)
 }
 
-func (vfs *virtualFileSystem) GetAccessibleEntries(path string) vfs.Entries {
+func (v *virtualFileSystem) GetAccessibleEntries(path string) vfs.Entries {
 	return osVFS.GetAccessibleEntries(path)
 }
 
-func (vfs *virtualFileSystem) Stat(path string) vfs.FileInfo {
+func (v *virtualFileSystem) Stat(path string) vfs.FileInfo {
 	return osVFS.Stat(path)
 }
 
-func (vfs *virtualFileSystem) WalkDir(root string, walkFn vfs.WalkDirFunc) error {
+func (v *virtualFileSystem) WalkDir(root string, walkFn vfs.WalkDirFunc) error {
 	return osVFS.WalkDir(root, walkFn)
 }
 
-func (vfs *virtualFileSystem) Realpath(path string) string {
+func (v *virtualFileSystem) Realpath(path string) string {
 	return osVFS.Realpath(path)
 }
